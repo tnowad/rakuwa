737698cbb15be3dcf044c69a53cb75eb76138162
Nguyen Minh Tuan <tnowad@gmail.com>
2022-11-11 01:08:53 +0700

    add slide show

--- js/Slideshow.js
+++ js/Slideshow.js
@@ -1,20 +1,32 @@
 let slideIndex = 0
 const slide = document.querySelectorAll(".slide")
+const slideDot = document.querySelectorAll(".list-dot-slide-item")
 const showDiv = (n) => {
     for (let i = 0; i < slide.length; i++) {
         slide[i].style.display = 'none'
+        slideDot[i].classList.remove('active')
     }
     if (n == slide.length) {
         n = 0;
     }
     else if (n == -1) {
-        n = slide.length -1;
+        n = slide.length - 1;
     }
     console.log(n)
     slideIndex = n
     slide[n].style.display = "block"
+    slideDot[n].classList.add('active')
 }
 function plusDiv(n) {
     showDiv(slideIndex + n);
 }
 showDiv(slideIndex);
+
+function startAuto() {
+    let Auto;
+    clearInterval(Auto);
+    Auto = setInterval(function () {
+        plusDiv(1)
+    }, 3000);
+}
+startAuto()
