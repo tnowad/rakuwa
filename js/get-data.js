498b771fed7e8a341d0317cfb89d5ae18b5026d2
Nguyen Minh Tuan <tnowad@gmail.com>
2022-11-23 16:44:01 +0700

    add method get data

--- js/get-data.js
+++ js/get-data.js
@@ -1,15 +1,52 @@
 import { fetchJson } from "./util"
 /**
  * Get data from server add store local
- * key: users, products, comments, cart
+ * key: users, products, comments, carts
  * type: user {id, username, password, email, address, payment, role, name, dateOfBirth, phoneNumber}
  * type: product {id, title, price, quantity, description, image, rating {rate, count}}
  * type: comment {productId, userId, body}
+ * type: cart {id, userId, products[id, quantity, price], amount}
  */
 
-const updateDataFromServer = () => {
+const updateLocalDataFromServer = async () => {
     let users = []
+    let products = []
+    let comments = []
+    let carts = []
     if (localStorage.getItem('users')) {
-        users = fetchJson('/api/v1/users.json')
+        users = await fetchJson('/api/v1/users.json')
+        localStorage.setItem('users', JSON.stringify(users))
     }
+    if (localStorage.getItem('products')) {
+        products = await fetchJson('/api/v1/products.json')
+        localStorage.setItem('products', JSON.stringify(products))
+    }
+    if (localStorage.getItem('comments')) {
+        comments = await fetchJson('/api/v1/comments.json')
+        localStorage.setItem('comments', JSON.stringify(comments))
+    }
+    if (localStorage.getItem('carts')) {
+        carts = await fetchJson('/api/v1/carts.json')
+        localStorage.setItem('comments', JSON.stringify(carts))
+    }
+}
+
+const getDataFromLocal = () => {
+    let users = JSON.parse(localStorage.getItem('users'))
+    let products = JSON.parse(localStorage.getItem('products'))
+    let comments = JSON.parse(localStorage.getItem('comments'))
+    let carts = JSON.parse(localStorage.getItem('carts'))
+    return { users, products, comments, carts }
+}
+
+let { users, products, comments, carts } = async () => {
+    await updateLocalDataFromServer()
+    return getDataFromLocal()
+}
+
+export {
+    users,
+    products,
+    comments,
+    carts
 }
