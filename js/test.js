ab4b11265e9268b44388b8618f8db7c3d982f77e
Nguyen Minh Tuan <tnowad@gmail.com>
2022-11-27 06:57:28 +0700

    add filter product

--- js/test.js
+++ js/test.js
@@ -1,13 +1,4 @@
-import { createProduct, updateProduct } from "./server/product.js"
+import { createProduct, updateProduct, getCategory, getProducts } from "./util/product.js"
 
-updateProduct({
-    "amount": 0,
-    "category": "meat",
-    "description": "Ex esse aliquip cillum esse excepteur dolor cillum ullamco. Exercitation dolore proident enim sint veniam culpa pariatur laboris. Pariatur aliquip cillum amet minim aute.",
-    "id": 1,
-    "image": "/img/products/thit-bo.png",
-    "label": "hot",
-    "price": "200000",
-    "rating": 5,
-    "title": "Thịt Bò"
-})
+console.log(getCategory())
+console.log(getProducts('meat', {low:1, high:100000}))
--- js/util/product.js
+++ js/util/product.js
@@ -10,7 +10,7 @@ const createProduct = (product) => {
 
 const updateProduct = (product) => {
     for (const i = 0; i < products.length; i++) {
-        if(products[i].id == product.id) {
+        if (products[i].id == product.id) {
             products[i] == product
             console.log(products[i], pro)
             break
@@ -19,7 +19,30 @@ const updateProduct = (product) => {
     localStorage.setItem('products', JSON.stringify(products))
 }
 
+const getProducts = (category, price) => {
+    if (category != undefined)
+        products = products.filter(product => product.category === category)
+    if (price != undefined) {
+        if (price.low != undefined)
+            products = products.filter(product => product.price >= price.low)
+        if (price.high != undefined)
+            products = products.filter(product => product.price <= price.high)
+    }
+
+    return products
+}
+
+const getCategory = () => {
+    let category = {}
+    products.forEach((value) => {
+        category[value.category] = category[value.category] ? category[value.category] + 1 : 1
+    })
+    return category
+}
+
 export {
     createProduct,
-    updateProduct
+    updateProduct,
+    getProducts,
+    getCategory
 }
