be387586050fb7644e1c043c72547e7dc78cb9ed
Nguyen Minh Tuan <tnowad@gmail.com>
2022-11-29 16:57:59 +0700

    add check login required

--- js/etc/test.js
+++ js/etc/test.js
@@ -1,10 +1,2 @@
-// import { html, render } from '/node_modules/lit-html/lit-html.js'
-
-// const myTemplate = (names) =>
-// 	html`
-// 		<ul>
-// 			${names.map((item) => html`<li>${item}</li>`)}
-// 		</ul>
-// 	`
-
-// render(myTemplate(['Bao ml', 'Tuấn']), document.body)
+import { loginRequired } from "../util/account.js";
+loginRequired()
