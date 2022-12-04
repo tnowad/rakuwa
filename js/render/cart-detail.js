0c60efca0bf65447d55c26e53d78eed19e6f50b8
Nguyen Minh Tuan <tnowad@gmail.com>
2022-12-05 00:18:56 +0700

    add change quantity

--- js/render/cart-detail.js
+++ js/render/cart-detail.js
@@ -8,3 +8,5 @@ const render = async () => {
 	}, '')
 }
 await render()
+
+export { render }
--- js/template/cart.js
+++ js/template/cart.js
@@ -1,28 +1,35 @@
 import { getTotalPriceProductIdInCart } from '../util/cart.js'
+import { addProductIdToCart } from '../util/cart.js'
+import { render } from '../render/cart-detail.js'
+
+const updateCart = () => {
+	setTimeout(render, 0)
+}
 
 const cart = (product) => {
 	return /* html */ `
         <tr>
-            <form action="" method="post">
-
-                <td><img src="${product.image}"></td>
-                <td>${product.title}</td>
-                <td>${product.price} VND</td>
-                <td>
-                <input
-                    type="number"
-                    name=""
-                    id=""
-                    placeholder =${product.quantity}>
+            <td><img src="${product.image}"></td>
+            <td>${product.title}</td>
+            <td>${product.price} VND</td>
+            <td>
+            <input
+                type="number"
+                min = 1
+                max = ${product.amount}
+                value = ${product.quantity}
+                onchange="
+                    addProductIdToCart(${product.id}, this.value); updateCart()"
+                >
             </td>
             <td>${product.quantity * product.price}</td>
             <td>
                 <button class="delete"><i class="fas fa-x"></i></button>
-
             </td>
-            </form>
         </tr>
     `
 }
+window.updateCart = updateCart
+window.addProductIdToCart = addProductIdToCart
 
 export { cart }
