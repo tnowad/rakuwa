06b8ae3e8f90e99ca29d7c203e8aa783fb7b677e
Nguyen Minh Tuan <tnowad@gmail.com>
2022-12-06 09:48:05 +0700

    add account template

---
+++ js/template/dashboard-table-row-product.js
@@ -0,0 +1,38 @@
+const productRow = (product) => {
+	return /* html */ `
+        <tbody class="table-item">
+			<tr class="table-item-top">
+				<td>${product.id}</td>
+				<td>${product.title}</td>
+				<td><img src="${product.image}" alt=""></td>
+				<td>${product.amount}</td>
+				<td>${product.price}</td>
+				<td>
+					<button
+						onclick="
+							this
+								.parentElement
+								.parentElement
+								.parentElement
+								.querySelector('.table-item-bottom')
+								.classList
+								.toggle('active')"
+						class="fas fa-edit"
+					></button>
+					<button
+						class="fa fa-trash"
+					></button>
+				</td>
+			</tr>
+			<tr>
+				<td
+					class="table-item-bottom"
+					colspan="6"
+				>
+					<form action="">123</form>
+				</td>
+			</tr>
+		</tbody>
+    `
+}
+export { productRow }
