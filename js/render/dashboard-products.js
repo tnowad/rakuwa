fce2e6afde31a7f1f02b4f693c61b3115a7725d8
Nguyen Minh Tuan <tnowad@gmail.com>
2022-12-06 09:54:30 +0700

    load product

--- js/render/dashboard-products.js
+++ js/render/dashboard-products.js
@@ -1,7 +1,14 @@
-const render = () => {
+import { getProducts } from '../util/product.js'
+import { productRow } from '../template/dashboard-table-row-product.js'
+const render = async () => {
 	const tableDashboardProduct = document.querySelector(
 		'.content.dashboard-products .table-container',
 	)
-	console.log(tableDashboardProduct)
+	const products = await getProducts()
+	tableDashboardProduct.innerHTML = products.reduce(
+		(previousValue, currentValue) =>
+			previousValue + productRow(currentValue),
+		'',
+	)
 }
 render()
