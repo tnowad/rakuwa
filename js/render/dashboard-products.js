77ebf196a74be311a16b305a9b5eb9d81d143a61
Nguyen Minh Tuan <tnowad@gmail.com>
2022-12-06 07:25:00 +0700

    add visable

--- js/render/dashboard-products.js
+++ js/render/dashboard-products.js
@@ -1,8 +1,7 @@
 const render = () => {
 	const tableDashboardProduct = document.querySelector(
 		'.content.dashboard-products .table-container',
-    )
-    tableDashboardProduct.innerHTML = 
+	)
 	console.log(tableDashboardProduct)
 }
 render()
