395d8c2260dba153a25228ec7a7ac357c1af92d1
Nguyen Minh Tuan <tnowad@gmail.com>
2022-12-06 07:24:24 +0700

    div page

--- js/render/dashboard-info.js
+++
@@ -1,3 +0,0 @@
-import { loginRequired, checkPermission } from '../util/account.js'
-
-const render = () => {}
---
+++ js/render/dashboard-products.js
@@ -0,0 +1,8 @@
+const render = () => {
+	const tableDashboardProduct = document.querySelector(
+		'.content.dashboard-products .table-container',
+    )
+    tableDashboardProduct.innerHTML = 
+	console.log(tableDashboardProduct)
+}
+render()
