2516342d7cb5089e053e35b2471544082b4e3e85
Nguyen Minh Tuan <tnowad@gmail.com>
2022-12-07 16:00:10 +0700

    add profile user

--- js/render/profile-user.js
+++ js/render/profile-user.js
@@ -1,6 +1,6 @@
 import { getDataFromLocal } from '../util/local-data.js'
 const render = async () => {
 	const { currentUser } = await getDataFromLocal()
-    
+	console.log(currentUser)
 }
 render()
