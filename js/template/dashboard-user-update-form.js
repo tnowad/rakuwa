bf4008e1483b934e629190f0caeb26917222e432
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2022-12-08 19:48:06 +0700

    fix user  manager

--- js/template/dashboard-user-update-form.js
+++ js/template/dashboard-user-update-form.js
@@ -2,23 +2,23 @@ const userForm = (user) => {
 	return `
 	<form class="form-action-user" onsubmit="return false">
 			<div class="group-form-edit-user edit-fullName" >
-				<label for="" id="fullName-Text" >Thay đổi tên</label>
+				<label for="" id="fullName-Text" >Họ tên</label>
 				<input type="text" id="fullName" value="${user.fullName}">
 			</div>
 			<div class="group-form-edit edit-dateOfBirth">
-				<label for="">Thay đổi ngày sinh </label>
+				<label for="">Ngày sinh </label>
 				<input type="date" name="" id="dateOfBirth" value="${user.dateOfBirth}">
 			</div>
 			<div class="group-form-edit edit-email">
-				<label for="">Thay đổi email</label>
+				<label for="">Email</label>
 				<input type="text" name="" id="email" value="${user.email}">
 				</div>
 			<div class="group-form-edit edit-image">
-				<label for="">Thay đổi ảnh</label>
+				<label for="">Ảnh</label>
 				<input type="file" name="" id="image" value="${user.image}">
 			</div>
 			<div class="group-form-edit edit-phone">
-				<label for="">Thay đổi số điện thoại</label>
+				<label for="">Số điện thoại</label>
 				<input type="text" name="" id="phone" value="${user.phoneNumber}">
 			</div>
 			<select name="gender" id="gender">
@@ -27,15 +27,15 @@ const userForm = (user) => {
 				<option value="order">Order</option>
  			</select>
 			<div class="group-form-edit edit-address">
-			<label for="">Thay đổi địa chỉ</label>
+			<label for="">Địa chỉ</label>
 				<textarea id="address" name="w3review" rows="4" cols="50">${user.address}</textarea>
 			</div>
 			<div class="group-form-edit edit-role" >
-				<label for="">Thay đổi quyền</label>
+				<label for="">Quyền truy cập</label>
 				<input type="option" id="role" value="${user.role}">
 			</div>
 			<div class="group-form-edit edit-password" >
-				<label for="">Thay đổi mật khẩu</label>
+				<label for="">Mật khẩu</label>
 				<input type="text" id="password" value="${user.password}">
 			</div>
 			<div class="edit-option">
