bdead9766499ef8e43566965864980825cb93b2e
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2022-12-08 20:12:19 +0700

    fix user  manager

--- js/template/dashboard-user-update-form.js
+++ js/template/dashboard-user-update-form.js
@@ -5,40 +5,45 @@ const userForm = (user) => {
 				<label for="" id="fullName-Text" >Họ tên</label>
 				<input type="text" id="fullName" value="${user.fullName}">
 			</div>
-			<div class="group-form-edit edit-dateOfBirth">
+			<div class="group-form-editUser edit-dateOfBirth">
 				<label for="">Ngày sinh </label>
 				<input type="date" name="" id="dateOfBirth" value="${user.dateOfBirth}">
 			</div>
-			<div class="group-form-edit edit-email">
+			<div class="group-form-editUser edit-email">
 				<label for="">Email</label>
 				<input type="text" name="" id="email" value="${user.email}">
 				</div>
-			<div class="group-form-edit edit-image">
+			<div class="group-form-editUser edit-image">
 				<label for="">Ảnh</label>
 				<input type="file" name="" id="image" value="${user.image}">
 			</div>
-			<div class="group-form-edit edit-phone">
+			<div class="group-form-editUser edit-phoneNumber">
 				<label for="">Số điện thoại</label>
 				<input type="text" name="" id="phone" value="${user.phoneNumber}">
 			</div>
-			<select name="gender" id="gender">
+			<select name="gender" class="group-form-editUser edit-gender">
 				<option value="male">Male</option>
 				<option value="female">Female</option>
 				<option value="order">Order</option>
  			</select>
-			<div class="group-form-edit edit-address">
+			<div class="group-form-editUser edit-address">
 			<label for="">Địa chỉ</label>
 				<textarea id="address" name="w3review" rows="4" cols="50">${user.address}</textarea>
 			</div>
-			<div class="group-form-edit edit-role" >
+			<div class="group-form-editUser edit-role" >
 				<label for="">Quyền truy cập</label>
 				<input type="option" id="role" value="${user.role}">
 			</div>
-			<div class="group-form-edit edit-password" >
+			<div class="group-form-editUser edit-password" >
 				<label for="">Mật khẩu</label>
 				<input type="text" id="password" value="${user.password}">
 			</div>
-			<div class="edit-option">
+			<select name="status" class="group-form-editUser edit-status">
+				<option value="active">Active</option>
+				<option value="ban">Ban</option>
+				<option value="deleted">Deleted</option>
+ 			</select>			
+			<div class="group-form-editUser edit-option-user">
 				<button id="cancel">Hủy</button>
 				<button id="submit" onclick="updateUser(this.parentElement.parentElement, ${user.id})" >Hoàn tất</button>
 				</div>
@@ -46,6 +51,6 @@ const userForm = (user) => {
 	`
 }
 window.updateUser = async () => {
-
+	1	
 }
 export { userForm }
