54194bb8c6f248f0b0cba26e75b43083f89aa9bc
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2022-12-08 10:02:53 +0700

    fix form js

--- js/template/dashboard-product-update-form.js
+++ js/template/dashboard-product-update-form.js
@@ -1,41 +1,46 @@
-import { getDataFromLocal, updateLocalDataFromServer } from "../util/local-data.js"
+import {
+	getDataFromLocal,
+	updateLocalDataFromServer,
+} from '../util/local-data.js'
+import { getProducts } from '../util/product.js'
 const productForm = (product) => {
 	return `
     <form class="form-action" action="">
-				<div class="group-form-edit edit-name" >
-					<label for="">Thay đổi tên</label>
-					<input type="text" value="${product.title}">
-				</div>
+	<div class="group-form-edit edit-name" >
+	<label for="">Thay đổi tên</label>
+					<input type="text" id="name" value="${product.title}">
+					</div>
 				<div class="group-form-edit edit-tag">
-					<label for="">Thay đổi thẻ</label>
-					<input type="text" name="" id="" value="${product.category}">
+				<label onclick="updateProduct()" for="">Thay đổi thẻ</label>
+				<input type="text" name="" id="" value="${product.category}">
 				</div>
 				<div class="group-form-edit edit-picture">
 				<label for="">Thay đổi ảnh</label>
 				<input type="file" name="" id="" value="${product.image}">
 				</div>
 				<div class="group-form-edit edit-amount">
-					<label for="">Thay đổi số lượng </label>
-					<input type="text" name="" id="" value="${product.amount}">
+				<label for="">Thay đổi số lượng </label>
+				<input type="text" name="" id="" value="${product.amount}">
 				</div>
 				<div class="group-form-edit edit price">
 					<label for="">Thay đổi giá</label>
 					<input type="text" name="" id="" value="${product.price}">
-				</div>
+					</div>
 			<div class="edit-describe">
 				<label for="">Thay đổi miêu tả</label>
 				<input type="text" name="" id="">
 			</div>
 			<div class="edit-option">
-				<button id="submit" onclick="updateProduct()">Hoàn tất</button>				
-				<button id="cancel">Hủy</button>
+			<button id="cancel">Hủy</button>
+			<button id="submit" onclick="${updateProduct(product.id)} ">Hoàn tất</button>				
 			</div>
 		</form>
     `
 }
-window.updateProduct = () => {
-	// console.log(54)
-	
+window.updateProduct = async (product) => {
+	const listProduct = await getProducts()
+	console.log(product)
+	// updateProduct(product)
 }
 
 export { productForm }
