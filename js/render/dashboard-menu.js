774f76e81b078d8fdf018600fcbf10619ba13051
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2022-12-09 20:26:29 +0700

    thong ke

--- js/render/dashboard-menu.js
+++ js/render/dashboard-menu.js
@@ -3,11 +3,14 @@ import { getDataFromLocal } from "../util/local-data.js"
 
 const render = async () => { 
     let totalPrice = document.querySelector('.content-statistical .left-statistical #turnover')
-    let { carts } = await getDataFromLocal()
+    let amountProduct = document.querySelector('.content-statistical .center-statistical #amountProduct')
+    let { carts,products } = await getDataFromLocal()
     let turnover = 0
     carts.map((carts) => turnover += carts.total)
-    console.log(turnover)
-    totalPrice.innerHTML =  new Intl.NumberFormat('ja-JP').format(turnover) + " VNĐ"
+    // console.log(turnover)
+    totalPrice.innerHTML = new Intl.NumberFormat('ja-JP').format(turnover) + " VNĐ"
+    let amount = 0
+    products.map((products) => amount ++)
 }
 
 render()
