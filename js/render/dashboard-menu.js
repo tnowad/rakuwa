b1ccd6ba2863131b8d03cc5865727a152565461a
Nguyen Minh Tuan <tnowad@gmail.com>
2022-12-10 17:22:08 +0700

    just plus paid orders

--- js/render/dashboard-menu.js
+++ js/render/dashboard-menu.js
@@ -1,16 +1,28 @@
-import { getDataFromLocal } from "../util/local-data.js"
-
+import { getDataFromLocal } from '../util/local-data.js'
 
 const render = async () => {
-    let totalPrice = document.querySelector('.content-statistical .left-statistical #turnover')
-    let amountProduct = document.querySelector('.content-statistical .center-statistical #amountProduct')
-    let amountUser = document.querySelector('.content-statistical .right-statistical #amountUser')
-    let { carts, products,users } = await getDataFromLocal()
-    let turnover = 0
-    carts.map((carts) => turnover += carts.total)
-    totalPrice.innerHTML = new Intl.NumberFormat('ja-JP').format(turnover) + " VNĐ"
-    amountProduct.innerHTML = products.length 
-    amountUser.innerHTML = users.length 
+	let totalPrice = document.querySelector(
+		'.content-statistical .left-statistical #turnover',
+	)
+	let amountProduct = document.querySelector(
+		'.content-statistical .center-statistical #amountProduct',
+	)
+	let amountUser = document.querySelector(
+		'.content-statistical .right-statistical #amountUser',
+	)
+	let { carts, products, users } = await getDataFromLocal()
+	let turnover = 0
+	// carts.map((carts) => (turnover += carts.total))
+	turnover = carts.reduce(
+		(previousValue, currentValue) =>
+			previousValue +
+			(currentValue.status == 'Thành công' ? currentValue.total : 0),
+		0,
+	)
+	totalPrice.innerHTML =
+		new Intl.NumberFormat('ja-JP').format(turnover) + ' VNĐ'
+	amountProduct.innerHTML = products.length
+	amountUser.innerHTML = users.length
 }
 
 render()
