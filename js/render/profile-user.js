919c0a16f0addde9553a8f773cb7596ecbcc4824
Nguyen Minh Tuan <tnowad@gmail.com>
2022-12-14 05:12:22 +0700

    fix bug

--- js/render/profile-user.js
+++ js/render/profile-user.js
@@ -3,7 +3,9 @@ const render = async () => {
 	const { currentUser } = await getDataFromLocal()
 	const userImage = document.querySelector('#user-image')
 	const userFullName = document.querySelector('#user-full-name')
+	const userGender = document.querySelector('#profile-user-gender')
 	userImage.src = currentUser.image
 	userFullName.textContent = currentUser.fullName
+	userGender.value = currentUser.value
 }
 render()
